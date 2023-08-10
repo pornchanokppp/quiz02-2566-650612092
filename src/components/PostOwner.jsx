dcc95c65b74ab08ec9fea67b1477493c91f057f8
pondckaokppp <pondchanokpp2003@gmail.com>
2023-08-10 15:30:45 +0700

    2

--- src/components/PostOwner.jsx
+++ src/components/PostOwner.jsx
@@ -1,3 +1,21 @@
 export const PostOwner = ({}) => {
-  return <div>Your code here...</div>;
+  return;
+  <div>
+    <img
+      src="public/profileImages/IMG_9424.JPG"
+      alt=""
+      width="50px"
+      className="border-radius: 50"
+    />
+    <h1 style="margin-left:80px"> Pornchanok Punin 650612092</h1>
+    <br />
+    <p style="margin-left:auto"> อยากมีแฟน </p>
+    <img
+      src="public/profileImages/ดาวน์โหลด.png"
+      alt=""
+      width="20px"
+      className="border-radius:50"
+    />
+    <p> 50 คน</p>
+  </div>;
 };
