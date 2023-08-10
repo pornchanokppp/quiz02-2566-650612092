bb3d2884e4204aa22134c4cf9b4ffc96863ae523
pondckaokppp <pondchanokpp2003@gmail.com>
2023-08-10 15:30:51 +0700

    3

--- src/components/Reply.jsx
+++ src/components/Reply.jsx
@@ -1,3 +1,14 @@
 export const Reply = ({ userImagePath, username, replyText, likeNum }) => {
-  return <div>your code here...</div>;
+  return (
+    <div>
+      <div className="d-flex border-radius:50">
+        {" "}
+        userImagePath :{userImagePath}{" "}
+      </div>
+      <div className="d-flex"> username :{username} </div>
+      <div className="d-flex"> replyText :{replyText} </div>
+      <div className="d-flex">likeNum :{likeNum} </div>
+      <div className="d-flex">replies :{replies} </div>
+    </div>
+  );
 };
