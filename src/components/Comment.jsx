e22891ec681db4fbcb29b3f6a3cb5e533d6aaaea
pondckaokppp <pondchanokpp2003@gmail.com>
2023-08-10 15:30:38 +0700

    1

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -7,7 +7,15 @@ export const Comment = ({
 }) => {
   return (
     <div>
-      your code here...
+      <div className="d-flex border-radius:50">
+        {" "}
+        userImagePath :{userImagePath}{" "}
+      </div>
+      <div className="d-flex"> username :{username} </div>
+      <div className="d-flex"> commentText :{commentText} </div>
+      <div className="d-flex">likeNum :{likeNum} </div>
+      <div className="d-flex">replies :{replies} </div>
+
       {/* map-loop render Reply component here */}
     </div>
   );
